d576a29bdea3e3bb06393e979a0d187feb69bd10
HipWitit <meshiabedard@gmail.com>
2026-02-25 14:55:34 -0800

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ st.set_page_config(page_title="Cypher Lite", layout="centered")
 
 st.markdown(f"""
     <link rel="manifest" href="https://raw.githubusercontent.com/HipWitit/cypherv4/main/manifest.json?v=2">
-    <link rel="icon" href="https://raw.githubusercontent.com/HipWitit/cypher-v4/main/appicon.png">
+    <link rel="icon" href="https://raw.githubusercontent.com/HipWitit/cypherv4/main/appicon.png">
     <meta name="mobile-web-app-capable" content="yes">
     <meta name="apple-mobile-web-app-capable" content="yes">
     <meta name="theme-color" content="#B4A7D6">
